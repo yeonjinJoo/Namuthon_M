fbf4e282a46c004b5199bacea0319a4a2453fc15
hyhy0623 <rlagkdus0623@naver.com>
2023-12-29 07:01:09 +0900

    fragment 연결, nav bar 구현 구글맵 api 연결(오류남)

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -33,6 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    viewBinding {
+        enable = true
+    }
 }
 
 dependencies {
@@ -44,4 +48,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.google.android.material:material:1.4.0")
+
+    // Google Map
+    implementation ("com.google.android.gms:play-services-maps:18.0.2")
+
+
 }
